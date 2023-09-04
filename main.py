33c4bddeda8e9c35dfc9d5a296de6a1c0cd0b28f
SeuSirigueijo <jgabriel4560@gmail.com>
2023-09-03 23:05:02 -0300

    03-09-2023

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-import datetime
+from flask import Flask, render_template, request, redirect, session, url_for, flash
+import datetime, secrets
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(24)
 
 data_atual = datetime.datetime.now().strftime("%d-%m-%Y")
 
@@ -22,13 +23,26 @@ def home():
 def login():
     if request.method == "POST":
         username = request.form.get("username")
-        password = request.form.get("password")
+        password =  request.form.get("password")
         if verificarCredenciais(username, password):
-            return render_template("carta.html")
+            session["username"] = username
+            session["password"] = password
+            return redirect(url_for("enviarCarta"))
+        else:
+            flash("Usuário ou senha incorretos.", "error")
+            return redirect(url_for("home"))
     else:
         print("deu errado")
         return redirect(url_for("home"))
 
+@app.route("/carta")
+def enviarCarta():
+    username = session.get("username")
+    password = session.get("password")
+    if username:
+        return render_template("carta.html", username=username, password=password, data_atual=data_atual)
+    else:
+        flash("Você precisa fazer login para acessar essa página.", "error")
 
 if __name__ == "__main__":
     app.run(debug = True)
